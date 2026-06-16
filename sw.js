5d532ee4185cf74dbc8571247261b1f36ebaa336
wlsgks189-debug <wlsgks189@gmail.com>
2026-06-16 15:48:58 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = 'exam-v1';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE)));
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
